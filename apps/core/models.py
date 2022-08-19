7337827f1284baf1771efd3ec89aee2b59cca5c9
SebastianHO <bastianhdezo@gmail.com>
2022-08-19 16:12:35 -0500

    apps: core

---
+++ apps/core/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class BaseModel(models.Model):
+    id = models.AutoField(primary_key=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
+    deleted_at = models.DateTimeField(default=None, blank=True, null=True, db_index=True)
+
+    class Meta:
+        abstract = True
+        ordering = ["-created_at"]
+        app_label = "website"
